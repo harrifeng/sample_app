d44e50316a5d23dc888dfc3b0c3cdfb8e0f54cf8
hfeng <haoran.feng@sap.com>
2014-08-21 18:37:22 +0800

    on the way to implement the session, but face the flash bug error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # 
+    else
+      flash[:error] = 'Invalid email/password combination' # Not quite right
+      render 'new'
+    end
   end
 
   def destroy
