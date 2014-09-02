ffb98a9124794e51270bd23b1d2dd9e52a4c7342
hfeng <haoran.feng@sap.com>
2014-09-02 17:16:56 +0800

    finish before part 9.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -56,5 +63,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
